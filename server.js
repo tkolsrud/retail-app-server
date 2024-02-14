9efe7a3f9f567d090856517541511340d215e00a
tkolsrud <thomaskolsrud@gmail.com>
2024-02-14 11:41:10 -0800

    creates express app and installs middleware

---
+++ server.js
@@ -0,0 +1,17 @@
+// npm packages
+import 'dotenv/config.js'
+import express from 'express'
+import logger from 'morgan'
+import cors from 'cors'
+import formData from 'express-form-data'
+
+// create express app
+const app = express()
+
+// middleware
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(formData.parse())
+
+export { app }
