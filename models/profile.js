f795af38125da1147ab1138d48e29fda6493bf58
tkolsrud <thomaskolsrud@gmail.com>
2024-02-20 11:52:53 -0800

    add fakestore API id to productSchema

--- models/profile.js
+++ models/profile.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const productSchema = new Schema ({
+  fakestoreId: Number,
   title: String,
   price: Number,
   description: String,
